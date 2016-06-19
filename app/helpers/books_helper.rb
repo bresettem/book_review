cfab7f83c3fbf96c5355a9aa2041c32510362b3f
Matthew Bresette <bresettem@gmail.com>
2016-06-19 18:33:14 +0000

    Fixed error on updating books and added image overlay to show page

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -59,9 +59,9 @@ module BooksHelper
   
   def show_missing_image()
     if @book.image_link.blank?
-      link_to (image_tag "missing.svg", class: 'center-block missing-book'), @book.info_link, :target => '_blank'
+      link_to (image_tag "missing.svg", class: 'center-block missing-book caption-img'), @book.info_link, :target => '_blank'
     else
-      link_to (image_tag @book.image_link(:medium), class: 'center-block thumbnail first-image'), @book.info_link, :target => '_blank'
+      link_to (image_tag @book.image_link(:medium), class: 'center-block img-thumbnail first-image caption-img'), @book.info_link, :target => '_blank'
       #link_to (image_tag 'google_preview.gif', class: 'center-block thumbnail second-image'), @book.info_link, :target => '_blank'
     end
   end
