0586d9b8d29977302e43d3edbe62bbc4eec790b7
Matthew Bresette <bresettem@gmail.com>
2016-07-10 19:03:26 +0000

    Checked for empty strings for authors and categories

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
 	def index
-		@authors = Book.select(:authors).where.not(:authors => nil).distinct
+		@authors = Book.select(:authors).where.not(:authors => nil, :authors => "").distinct
 	end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
 	def index
-		@categories = Book.select(:categories).where.not(:categories => nil).distinct
+		@categories = Book.select(:categories).where.not(:categories => nil, :categories => "").distinct
 	end
 end
