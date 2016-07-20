09e7ac9b62a23c0b462ce9fc258abfc8567d7d0f
Matthew Bresette <bresettem@gmail.com>
2016-07-20 01:02:57 +0000

    Fixed responsiveness

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -48,22 +48,22 @@ module BooksHelper
   end
   
   def isbn(result) 
-    result.isbn ? result.isbn : "No ISBN is Available"
+    (result.isbn ? result.isbn : "No ISBN is Available")
   end
   
   def missing_image(a)
     if a.image_link.blank?
-      link_to (image_tag "missing.svg", class: 'center-block missing-book'), book_path(a)
+      link_to (image_tag "missing.svg", class: 'img-thumbnail missing-book caption-img'), book_path(a)
     else
-    	link_to (image_tag a.image_link(:medium), class: 'center-block img-thumbnail caption-img'), book_path(a)
+    	link_to (image_tag a.image_link(:medium), class: 'img-thumbnail caption-img'), book_path(a)
     end
   end
   
   def show_missing_image(book)
     if book.image_link.blank?
-      link_to (image_tag "missing.svg", class: 'center-block missing-book caption-img'), book.info_link, :target => '_blank'
+      link_to (image_tag "missing.svg", class: 'img-thumbnail missing-book caption-img'), book.info_link, :target => '_blank'
     else
-      link_to (image_tag book.image_link(:medium), class: 'center-block img-thumbnail first-image caption-img'), book.info_link, :target => '_blank'
+      link_to (image_tag book.image_link(:medium), class: 'img-thumbnail first-image caption-img'), book.info_link, :target => '_blank'
     end
   end
 end
