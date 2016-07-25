8ad197442e08f8f8e844ce1636cf5eedb51fb269
Matthew Bresette <bresettem@gmail.com>
2016-07-25 01:44:55 +0000

    Added benchmark

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# require 'faker'
+require 'benchmark'
+start_books = Book.count
+num_times = 5
 books=[]
-5.times do |search|
-  search = GoogleBooks.search(Faker::Book.title, :count => 40)
-  search.each do |a|
-    books << a
-  end
+
+Benchmark.bm do |x|
+  x.report { 
+    Book.transaction do
+      num_times.times do |search|
+        random = Faker::Lorem.word
+        search = GoogleBooks.search(random, :count => 40)
+        Book.transaction do
+          search.each do |result|
+            books << result
+          end
+        end
+      end
+     end
+  }
+
+  x.report { 
+    books.each do |result|
+      Book.create(
+        books_id: result.id,
+        image_link: result.image_link,
+        title: result.title,
+        authors: result.authors,
+        publisher: result.publisher,
+        published_date: result.published_date,
+        description: result.description,
+        isbn: result.isbn,
+        page_count: result.page_count,
+        categories: result.categories,
+        average_rating: result.average_rating,
+        ratings_count: result.ratings_count,
+        preview_link: result.preview_link,
+        info_link: result.info_link,
+        user_id: 1
+      )
+    end
+  }
 end
-books.each do |result|
-  Book.create(
-    books_id: result.id,
-    image_link: result.image_link,
-    title: result.title,
-    authors: result.authors,
-    publisher: result.publisher,
-    published_date: result.published_date,
-    description: result.description,
-    isbn: result.isbn,
-    page_count: result.page_count,
-    categories: result.categories,
-    average_rating: result.average_rating,
-    ratings_count: result.ratings_count,
-    preview_link: result.preview_link,
-    info_link: result.info_link,
-    user_id: 1
-  )
-end
+end_books=Book.count
+added_books = end_books - start_books
+p "Start: #{start_books} End: #{end_books} Added #{added_books}"
