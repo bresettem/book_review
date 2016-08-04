db2da4cccd1deaa4c63d304b6528da48291acdba
Matthew Bresette <bresettem@gmail.com>
2016-08-04 03:35:26 +0000

    Added images to aws

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,18 @@ class Book < ActiveRecord::Base
     validates :user_id, presence: true
     validates :books_id, :presence => true, :uniqueness => true
     
-    has_attached_file :image_link, styles: { medium: "128>", thumb: "100x100>" }
+    has_attached_file :image_link, styles: { medium: "128>", thumb: "100x100>" },
+                        :storage => :s3,
+                                   :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+    def s3_credentials
+      {
+        :bucket => ENV["AWS_S3_BUCKET"], 
+        :access_key_id => ENV["AWS_ACCESS_KEY_ID"], 
+        :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+        :s3_region => ENV["S3_REGION"]
+      }
+      
+    end 
     validates_attachment_content_type :image_link, content_type: /\Aimage\/.*\Z/
     
     before_save :destroy_image?
