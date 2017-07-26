c62174cde3e4f3e3499693ca310fcca5f2498bfc
Matthew Bresette <bresettem@gmail.com>
2017-07-26 23:07:16 +0000

    Updated rails to 5.0.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "railsdiff_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "book_review_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
- # Use a different logger for distributed setups.
+  # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
